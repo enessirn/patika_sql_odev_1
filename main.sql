0019166f25537747bb51cb1ab8ddfacbd8b9ca90
Enes SIRIN <enessirinbusiness@gmail.com>
2024-11-21 23:42:28 +0300

    odev 1 success

---
+++ main.sql
@@ -0,0 +1,18 @@
+-- 1.Soru Cevabı
+-- SELECT title,description FROM film;
+
+-- 2.Soru Cevabı
+-- SELECT * FROM film
+-- WHERE length > 60 AND length < 75;
+
+-- 3.Soru Cevabı
+-- SELECT * FROM film
+-- WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
+
+-- -- 4.Sorunun Cevabı
+-- SELECT first_name,last_name FROM customer
+-- WHERE first_name = 'Mary';
+
+-- 5.Sorunun Cevabı
+-- SELECT length,rental_rate FROM film
+-- WHERE ( NOT length > 50) AND (rental_rate != 2.99 AND rental_rate != 4.99);
